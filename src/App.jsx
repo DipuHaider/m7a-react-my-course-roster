73c9a18a0385aa9b916ba1b8c54fc0411c4a1786
Dipu Haider <dipuhaider@gmail.com>
2023-09-15 15:13:02 +0600

    Initial project setup with Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline">My Course Roaster</h1>
+      
+    </>
+  )
+}
+
+export default App
