9896404cf98289b820a42b3f37d16c7f84b9e7ef
Dipu Haider <dipuhaider@gmail.com>
2023-09-15 19:37:01 +0600

    Create Header component & add google fonts

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold bg-sky-800 text-white">Course Registration</h2>
+        </div>
+    );
+};
+
+export default Header;
