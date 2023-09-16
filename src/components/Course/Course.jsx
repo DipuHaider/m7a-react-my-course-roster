e0e35550efc1080b34cb69b5044e097197363037
Dipu Haider <dipuhaider@gmail.com>
2023-09-16 10:18:05 +0600

    Course All Data Show and Install React propTypes

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,14 +1,33 @@
+import PropTypes from 'prop-types';
+
 const Course = ({course}) => {
-    const{title, img} = course;
+    const{title, img, details, price, credit} = course;
 
     return (
         <div className="course">
-            <h3>Course: {title}</h3>
-            <img src={img} alt="" />
+            
             {/* <p>Price: {price}</p>
             <button onClick={() => handleAddToCart(bottle)}>Purchase</button> */}
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={img} alt={title} /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p>{details}</p>
+                    <div>
+                        <h3>Price: {price}</h3>
+                        <h3>Credit hours: {credit}</h3>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Select</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+Course.propTypes = {
+    course: PropTypes.object.isRequired,
+}
 
 export default Course;
