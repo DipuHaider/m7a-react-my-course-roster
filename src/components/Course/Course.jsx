a8ae523a8206ff38d8417fa592ee1fb7dd54ac0c
Dipu Haider <dipuhaider@gmail.com>
2023-09-16 09:47:32 +0600

    install DaisyUi

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -3,7 +3,7 @@ const Course = ({course}) => {
 
     return (
         <div className="course">
-            <h3>Bottle: {title}</h3>
+            <h3>Course: {title}</h3>
             <img src={img} alt="" />
             {/* <p>Price: {price}</p>
             <button onClick={() => handleAddToCart(bottle)}>Purchase</button> */}
