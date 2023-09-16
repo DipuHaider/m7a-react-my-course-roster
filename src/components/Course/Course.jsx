e8b7acb42aa7f61a41c3185fe365bb396b0b1790
Dipu Haider <dipuhaider@gmail.com>
2023-09-16 09:39:07 +0600

    Courses data show, Create Course component

---
+++ src/components/Course/Course.jsx
@@ -0,0 +1,14 @@
+const Course = ({course}) => {
+    const{title, img} = course;
+
+    return (
+        <div className="course">
+            <h3>Bottle: {title}</h3>
+            <img src={img} alt="" />
+            {/* <p>Price: {price}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button> */}
+        </div>
+    );
+};
+
+export default Course;
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Course from "../Course/Course";
+
 const Courses = () => {
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('courses.json').then(res => res.json()).then(data => setCourses(data));
+    }, []); 
+
     return (
         <div>
+            <h2>Courses Available: {courses.length}</h2>
             
+            <div className="course-container">
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                        ></Course>)
+                }
+            </div>
         </div>
     );
 };
